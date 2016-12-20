de4ab8c4333850d08ea7c54512ad95b9f3df2b95
mertiak <jljoorgee@gmail.com>
2016-12-20 19:14:49 +0100

    3#movimiento futbol

---
+++ Assets/Scripts/Mov_azul.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mov_azul : MonoBehaviour {
+	public float velocidad = 5f;
+	public float power = 1f;
+	public float vertical = 15f;
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent <Rigidbody2D> ();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			rb.velocity = new Vector2 (-velocidad * power, rb.velocity.y);
+		}
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			rb.velocity = new Vector2 (velocidad * power, rb.velocity.y);
+		}
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			rb.AddForce (transform.up*vertical);
+		}
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			rb.AddForce (transform.up*-vertical);
+		}
+	}
+}
